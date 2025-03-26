5f11dc684390e75f27c30297da2548580be24fc9
Julien Pibarot <pibar@DESKTOP-UHK8N5A>
2025-03-26 16:34:05 +0100

    feat: initial data

--- djangomizer/utils.py
+++ djangomizer/utils.py
@@ -3,11 +3,13 @@ import pandas as pd
 import os, sys
 #from randomizer.models import Jeux
 
-'''def insert_data():
+"../jeux.ods", engine="odf"
+
+def insert_data(ods_file):
     #insérer les données initiales
     from randomizer.models import Jeux
-    df = pd.read_excel("../jeux.ods", engine="odf")
+    df = pd.read_excel(ods_file)
     for index, row in df.iterrows():
         data = {col: row[col] for col in df.columns}
         Jeux.objects.create(**data)
-    print('Great success !')'''
+    print('Great success !')
--- randomizer/apps.py
+++ randomizer/apps.py
@@ -1,6 +1,25 @@
 from django.apps import AppConfig
+from django.core.management import call_command
+from django.db.models.signals import post_migrate
+
 
 class RandomizerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'randomizer'
 
+    def ready(self):
+        # Insérer des données dans la bdd après les migrations
+        post_migrate.connect(insert_initial_data, sender=self)
+
+def insert_initial_data(sender, **kwargs):
+    print("📌 Signal post_migrate déclenché !")
+
+    from .models import Jeux
+    from djangomizer.utils import insert_data
+
+    file = randomizer_jeux.ods
+
+    if Jeux.objects.count() == 0:
+        insert_data(file)
+
+    print(Jeux.objects.count())
