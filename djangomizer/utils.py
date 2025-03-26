a97e3bcb6281f0aecf05ae3f4818da5b6abbd51c
Julien Pibarot <pibar@DESKTOP-UHK8N5A>
2025-03-26 18:13:39 +0100

    fix: values

--- djangomizer/utils.py
+++ djangomizer/utils.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import numpy as np
 import os, sys
 #from randomizer.models import Jeux
 
@@ -9,6 +10,7 @@ def insert_data(ods_file):
     #insérer les données initiales
     from randomizer.models import Jeux
     df = pd.read_excel(ods_file)
+    df = df.replace({np.nan: None})
     for index, row in df.iterrows():
         data = {col: row[col] for col in df.columns}
         Jeux.objects.create(**data)
